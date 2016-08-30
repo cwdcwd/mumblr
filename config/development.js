5b86b378fdcdd2fc205484cc7ed8982629e46bd6
lazybaer <cwd@topcoder.com>
2016-08-30 04:19:02 +0000

    fleshing this out further

---
+++ config/development.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let config = {};
+
+config.KB_HOST = 'https://keybase.io';
+config.KB_API = '/_/api/1.0/';
+config.KB_BASEURL = config.KB_HOST + config.KB_API;
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,65 @@
 'use strict';
 
-let _=require('lodash');
+let _ = require('lodash');
+let config = require('config');
 let express = require('express');
 let router = express.Router();
-let qs=require('querystring');
-let config=require('config');
-let requestPromise=require('request-promise');
+var crypto = require('crypto')
+let qs = require('querystring');
+let requestPromise = require('request-promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let url='https://keybase.io/_/api/1.0/getsalt.json';
-  let username=req.query.username;
+    let url = config.KB_BASEURL + 'getsalt.json';
+    let username = req.query.username || '';
+    let password = req.query.password || '';
 
-  let opts={
-    method: 'GET',
-    uri: url+'?'+qs.stringify({email_or_username: username}),
-    headers: {
-      'Accept': 'application/json',
-    },
-    json: true
-  };
+    let opts = {
+        method: 'GET',
+        uri: url + '?' + qs.stringify({
+            email_or_username: username
+        }),
+        headers: {
+            'Accept': 'application/json',
+        },
+        json: true
+    };
 
-  requestPromise(opts).then(function(kbRes) {
-    res.json(kbRes);
-  }).catch(function(err) {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    requestPromise(opts).then(function(kbRes) {
+        //res.json(kbRes);
+        let url = config.KB_BASEURL + 'login.json';
+        var decodedSessionToken = new Buffer(kbRes.login_session, 'base64').toString('utf8');
+
+        let hash = crypto.createHmac('sha512', decodedSessionToken).update(password, 'utf8').digest(
+            'hex');
+        console.log(kbRes.login_session, decodedSessionToken);
+        console.log(hash);
+
+        let opts = {
+            method: 'POST',
+            uri: url,
+            headers: {
+                'Accept': 'application/json',
+                'X-CSRF-Token': kbRes.csrf_token
+            },
+            form: {
+                email_or_username: username,
+                hmac_pwh: hash,
+                login_session: kbRes.login_session
+            }
+        };
+
+        requestPromise(opts).then(function(kbLoginRes) {
+            let kbLog = JSON.parse(kbLoginRes);
+            res.json(kbLog);
+        }).catch(function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }).catch(function(err) {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
