0f95ce2563bf1fb3b47eb911673220ba201c1fcc
lazybaer <cwd@topcoder.com>
2016-08-30 03:39:42 +0000

    adding in first leg of login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+let _=require('lodash');
+let express = require('express');
+let router = express.Router();
+let qs=require('querystring');
+let config=require('config');
+let requestPromise=require('request-promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let url='https://keybase.io/_/api/1.0/getsalt.json';
+  let username=req.query.username;
+
+  let opts={
+    method: 'GET',
+    uri: url+'?'+qs.stringify({email_or_username: username}),
+    headers: {
+      'Accept': 'application/json',
+    },
+    json: true
+  };
+
+  requestPromise(opts).then(function(kbRes) {
+    res.json(kbRes);
+  }).catch(function(err) {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
